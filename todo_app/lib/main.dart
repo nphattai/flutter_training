07aeaf91ec04c7d659bf19347a095d5f9954045c
Nguyen Tai <phat-tai.nguyen@ant-tech.eu>
2021-01-29 13:59:03 +0700

    refactor code

--- todo_app/lib/main.dart
+++ todo_app/lib/main.dart
@@ -19,21 +19,23 @@ class ToDoList extends StatefulWidget {
 }
 
 class _TodoListState extends State<ToDoList> {
-  final _titleInputControler = TextEditingController();
-  final _descriptionInputControler = TextEditingController();
+  final TextEditingController _titleInputControler = TextEditingController();
+  final TextEditingController _descriptionInputControler =
+      TextEditingController();
 
-  final _todoList = <TodoItem>[];
-  final _doneList = <TodoItem>[];
-  final _noneList = <TodoItem>[];
+  List<TodoItem> _todoList = <TodoItem>[];
 
-  final uuid = Uuid();
+  final Uuid uuid = Uuid();
 
   List<TodoItem> _data;
+
+  String _option;
 
   @override
   void initState() {
     super.initState();
     _data = _todoList;
+    _option = 'all';
   }
 
   @override
@@ -48,12 +50,11 @@ class _TodoListState extends State<ToDoList> {
         _descriptionInputControler.text.trim() != '') {
       setState(() {
         final todo = TodoItem(
-          id: uuid.v4(),
-          title: _titleInputControler.text.trim(),
-          description: _descriptionInputControler.text.trim(),
-        );
+            id: uuid.v4(),
+            title: _titleInputControler.text.trim(),
+            description: _descriptionInputControler.text.trim(),
+            done: false);
         _todoList.add(todo);
-        _noneList.add(todo);
       });
     }
     _titleInputControler.text = '';
@@ -61,8 +62,6 @@ class _TodoListState extends State<ToDoList> {
   }
 
   Widget _buildTodoItem(TodoItem item) {
-    final done = _doneList.contains(item);
-
     return SizedBox(
       child: Card(
         child: Column(
@@ -70,25 +69,52 @@ class _TodoListState extends State<ToDoList> {
             ListTile(
               onTap: () {
                 setState(() {
-                  if (done == true) {
-                    _doneList.remove(item);
-                    _noneList.add(item);
-                  } else {
-                    _doneList.add(item);
-                    _noneList.remove(item);
+                  final index = _todoList.indexOf(item);
+
+                  _todoList = _todoList
+                      .where((element) => element.id != item.id)
+                      .toList();
+
+                  _todoList.insert(
+                    index,
+                    TodoItem(
+                      id: item.id,
+                      title: item.title,
+                      description: item.description,
+                      done: !item.done,
+                    ),
+                  );
+
+                  switch (_option) {
+                    case 'all':
+                      _data = _todoList;
+                      break;
+                    case 'done':
+                      _data = _todoList.where((todo) => todo.done).toList();
+                      break;
+                    case 'none':
+                      _data = _todoList.where((todo) => !todo.done).toList();
+                      break;
+                    default:
                   }
                 });
               },
               trailing: Icon(
-                done == true ? Icons.favorite : Icons.favorite_border,
+                item.done == true ? Icons.favorite : Icons.favorite_border,
                 color: Colors.white,
               ),
               tileColor: Colors.blue,
-              title: Text(item.title,
-                  style: TextStyle(fontWeight: FontWeight.w500)),
+              title: Text(
+                item.title,
+                style: TextStyle(
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
             ),
             ListTile(
-              title: Text(item.description),
+              title: Text(
+                item.description,
+              ),
             ),
           ],
         ),
@@ -103,62 +129,80 @@ class _TodoListState extends State<ToDoList> {
         title: Text('TODO APP'),
       ),
       body: Padding(
-          padding: const EdgeInsets.all(20),
-          child: Column(children: [
-            TextField(
-              controller: _titleInputControler,
-              decoration: InputDecoration(hintText: 'Title'),
+        padding: const EdgeInsets.all(20),
+        child: Column(children: [
+          TextField(
+            controller: _titleInputControler,
+            decoration: InputDecoration(hintText: 'Title'),
+          ),
+          TextField(
+            controller: _descriptionInputControler,
+            decoration: InputDecoration(
+              hintText: 'Description',
             ),
-            TextField(
-              controller: _descriptionInputControler,
-              decoration: InputDecoration(hintText: 'Description'),
+          ),
+          TextButton(
+            onPressed: _saveTodo,
+            child: Text(
+              'Submit',
             ),
-            TextButton(onPressed: _saveTodo, child: Text('Submit')),
-            Row(
-              children: [
-                TextButton(
-                    onPressed: () {
-                      setState(() {
-                        _data = _todoList;
-                      });
-                    },
-                    child: Text(
-                      'All',
-                      style: TextStyle(
-                          color: _data == _todoList ? Colors.red : Colors.blue),
-                    )),
-                TextButton(
-                    onPressed: () {
-                      setState(() {
-                        _data = _doneList;
-                      });
-                    },
-                    child: Text('Done',
-                        style: TextStyle(
-                            color: _data == _doneList
-                                ? Colors.red
-                                : Colors.blue))),
-                TextButton(
-                    onPressed: () {
-                      setState(() {
-                        _data = _noneList;
-                      });
-                    },
-                    child: Text('None',
-                        style: TextStyle(
-                            color: _data == _noneList
-                                ? Colors.red
-                                : Colors.blue))),
-              ],
-            ),
-            Expanded(
-                flex: 1,
-                child: ListView.builder(
-                    itemCount: _data.length,
-                    itemBuilder: (context, index) {
-                      return _buildTodoItem(_data[index]);
-                    }))
-          ])),
+          ),
+          Row(
+            children: [
+              TextButton(
+                onPressed: () {
+                  setState(() {
+                    _option = 'all';
+                    _data = _todoList;
+                  });
+                },
+                child: Text(
+                  'All',
+                  style: TextStyle(
+                    color: _option == 'all' ? Colors.red : Colors.blue,
+                  ),
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  setState(() {
+                    _option = 'done';
+                    _data = _todoList.where((todo) => todo.done).toList();
+                  });
+                },
+                child: Text(
+                  'Done',
+                  style: TextStyle(
+                    color: _option == 'done' ? Colors.red : Colors.blue,
+                  ),
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  setState(() {
+                    _option = 'none';
+                    _data = _todoList.where((todo) => !todo.done).toList();
+                  });
+                },
+                child: Text(
+                  'None',
+                  style: TextStyle(
+                    color: _option == 'none' ? Colors.red : Colors.blue,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Expanded(
+            flex: 1,
+            child: ListView.builder(
+                itemCount: _data.length,
+                itemBuilder: (context, index) {
+                  return _buildTodoItem(_data[index]);
+                }),
+          )
+        ]),
+      ),
     );
   }
 }
@@ -167,7 +211,11 @@ class TodoItem {
   final String id;
   final String title;
   final String description;
+  final bool done;
 
   const TodoItem(
-      {@required this.id, @required this.title, @required this.description});
+      {@required this.id,
+      @required this.title,
+      @required this.description,
+      @required this.done});
 }
