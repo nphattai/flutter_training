e3494f2d8ed74eb0b38226d3097d4ecb9ca5c832
Nguyen Tai <phat-tai.nguyen@ant-tech.eu>
2021-02-22 23:23:34 +0700

    refactor code

--- test_3/lib/main.dart
+++ test_3/lib/main.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:provider/provider.dart';
 import 'package:test_3/AppImages.dart';
 import 'package:test_3/models/location.dart';
 import 'package:test_3/models/results.dart';
@@ -14,33 +13,21 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider<ThemeModel>(
-      create: (_) => ThemeModel(),
-      child: Consumer<ThemeModel>(
-        builder: (_, ThemeModel model, __) {
-          return MaterialApp(
-            theme: ThemeData.light(), // Provide light theme.
-            darkTheme: ThemeData.dark(), // Provide dark theme.
-            themeMode: model.mode, // Decides which theme to show.
-            home: MyHomePage(model: model),
-          );
-        },
-      ),
+    return MaterialApp(
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key key, this.model}) : super(key: key);
-
-  final ThemeModel model;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
   Future<User> futureUser;
+
+  bool isLightMode = true;
 
   @override
   void initState() {
@@ -50,109 +37,123 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: FutureBuilder<User>(
-        future: futureUser,
-        builder: (BuildContext context, AsyncSnapshot<User> snapshot) {
-          final bool isLightMode = widget.model._mode == ThemeMode.light;
+    return MaterialApp(
+      theme: ThemeData.light(), // Provide light theme.
+      darkTheme: ThemeData.dark(), // Provide dark theme.
+      themeMode: isLightMode
+          ? ThemeMode.light
+          : ThemeMode.dark, // Decides which theme to show.
+      home: Scaffold(
+        body: FutureBuilder<User>(
+          future: futureUser,
+          builder: (BuildContext context, AsyncSnapshot<User> snapshot) {
+            if (snapshot.hasData) {
+              final Results results = snapshot.data.results[0];
+              final String avatar = results.picture.medium;
+              final String fullName = results.name.fullName;
+              final String email = results.email;
+              final String phoneNumber = results.phone;
+              final Location location = results.location;
+              final String address = location.address;
 
-          final String background =
-              isLightMode ? AppImages.morning : AppImages.night;
+              final String background =
+                  isLightMode ? AppImages.morning : AppImages.night;
 
-          final IconData iconData =
-              isLightMode ? Icons.nightlight_round : Icons.lightbulb_outlined;
+              final IconData iconData = isLightMode
+                  ? Icons.nightlight_round
+                  : Icons.lightbulb_outlined;
 
-          if (snapshot.hasData) {
-            final Results results = snapshot.data.results[0];
-            final String avatar = results.picture.medium;
-            final String fullName = results.name.fullName;
-            final String email = results.email;
-            final String phoneNumber = results.phone;
-            final Location location = results.location;
-            final String address = location.address;
-
-            return Column(
-              children: <Widget>[
-                Flexible(
-                  child: Stack(
-                    children: <Widget>[
-                      Container(
-                        padding: const EdgeInsets.only(top: 30),
-                        width: MediaQuery.of(context).size.width,
-                        decoration: BoxDecoration(
-                          image: DecorationImage(
-                              fit: BoxFit.cover, image: AssetImage(background)),
-                        ),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: <Widget>[
-                            CircleAvatar(
-                              radius: 45,
-                              backgroundImage: NetworkImage(
-                                avatar,
-                              ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.only(top: 10),
-                              child: Text(
-                                fullName,
-                                style: const TextStyle(
-                                  fontSize: 30,
+              return Column(
+                children: <Widget>[
+                  Flexible(
+                    child: Stack(
+                      children: <Widget>[
+                        Container(
+                          padding: const EdgeInsets.only(top: 30),
+                          width: MediaQuery.of(context).size.width,
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                                fit: BoxFit.cover,
+                                image: AssetImage(background)),
+                          ),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: <Widget>[
+                              CircleAvatar(
+                                radius: 45,
+                                backgroundImage: NetworkImage(
+                                  avatar,
                                 ),
                               ),
-                            ),
-                          ],
-                        ),
-                      ),
-                      Positioned(
-                        child: FlatButton(
-                          onPressed: () {
-                            widget.model.toggleMode();
-                          },
-                          child: Icon(
-                            iconData,
+                              Padding(
+                                padding: const EdgeInsets.only(top: 10),
+                                child: Text(
+                                  fullName,
+                                  style: const TextStyle(
+                                    fontSize: 30,
+                                  ),
+                                ),
+                              ),
+                            ],
                           ),
                         ),
-                        right: 0,
-                        top: 50,
-                      )
-                    ],
-                  ),
-                  flex: 3,
-                ),
-                Flexible(
-                  child: Container(
-                    padding: const EdgeInsets.only(top: 20),
-                    decoration: BoxDecoration(
-                        color: Theme.of(context).scaffoldBackgroundColor),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: <Widget>[
-                        Card(
-                          leading: Icons.email,
-                          content: email,
-                        ),
-                        Card(
-                          leading: Icons.phone,
-                          content: phoneNumber,
-                        ),
-                        Card(
-                          leading: Icons.map,
-                          content: address,
-                        ),
+                        Positioned(
+                          child: FlatButton(
+                            onPressed: () {
+                              setState(() {
+                                isLightMode = !isLightMode;
+                              });
+                            },
+                            child: Icon(
+                              iconData,
+                            ),
+                          ),
+                          right: 0,
+                          top: 50,
+                        )
                       ],
                     ),
+                    flex: 3,
                   ),
-                  flex: 7,
-                )
-              ],
-            );
-          } else if (snapshot.hasError) {
-            return Text('$snapshot.error');
-          }
-          return const CircularProgressIndicator();
-        },
+                  Flexible(
+                    child: Container(
+                      padding: const EdgeInsets.only(top: 20),
+                      decoration: BoxDecoration(
+                          color: Theme.of(context).scaffoldBackgroundColor),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: <Widget>[
+                          Card(
+                            leading: Icons.email,
+                            content: email,
+                          ),
+                          Card(
+                            leading: Icons.phone,
+                            content: phoneNumber,
+                          ),
+                          Card(
+                            leading: Icons.map,
+                            content: address,
+                          ),
+                        ],
+                      ),
+                    ),
+                    flex: 7,
+                  )
+                ],
+              );
+            } else if (snapshot.hasError) {
+              return Center(
+                child: Text(
+                  '$snapshot.error',
+                  style: const TextStyle(color: Colors.red),
+                ),
+              );
+            }
+            return const Center(child: CircularProgressIndicator());
+          },
+        ),
       ),
     );
   }
@@ -191,25 +192,19 @@ class Card extends StatelessWidget {
       ),
       child: Row(
         children: <Widget>[
-          Icon(leading),
           Padding(
-            padding: const EdgeInsets.only(left: 20),
-            child: Text(content),
-          )
+            padding: const EdgeInsets.only(right: 15),
+            child: Icon(leading),
+          ),
+          Expanded(
+            child: Text(
+              content,
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+            ),
+          ),
         ],
       ),
     );
-  }
-}
-
-class ThemeModel with ChangeNotifier {
-  ThemeModel({ThemeMode mode = ThemeMode.light}) : _mode = mode;
-
-  ThemeMode _mode;
-  ThemeMode get mode => _mode;
-
-  void toggleMode() {
-    _mode = _mode == ThemeMode.light ? ThemeMode.dark : ThemeMode.light;
-    notifyListeners();
   }
 }
